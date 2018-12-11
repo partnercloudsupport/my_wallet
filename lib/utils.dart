670fb199136f82ead80d6f09064ce7d9c9c983fb
suesitran <suesi.tran@cognizant.com>
2018-12-11 12:17:27 +1100

    Summary: common email validation method

--- lib/utils.dart
+++ lib/utils.dart
@@ -15,3 +15,15 @@ DateTime startOfDay(DateTime day) {
 DateTime endOfDay(DateTime day) {
   return DateTime(day.year, day.month, day.day).add(Duration(days: 1));
 }
+
+bool isEmailFormat(String email) {
+  RegExp reg = RegExp("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+      "\\@" +
+      "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+      "(" +
+      "\\." +
+      "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+      ")+");
+
+  return email.contains(reg);
+}
