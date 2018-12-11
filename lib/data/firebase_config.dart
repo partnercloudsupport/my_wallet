f2decf298367a3d576e3d28f1c8146da774b7306
suesitran <suesi.tran@cognizant.com>
2018-12-11 14:52:13 +1100

    Summary: Delete config

--- lib/data/firebase_config.dart
+++
@@ -1,7 +0,0 @@
-const firebase_ios_app_id = "1:990274208221:ios:ea83482f33c13796";
-const firebase_android_app_id = "1:990274208221:android:c528bc0846681362";
-
-const firebase_api_key = "AIzaSyBlJGFamuhZwlcAi_9BD3CjXgk2oDPVl4s";
-const firebase_gcm_sender_id = "990274208221";
-const firebase_project_id = "my-wallet-9438d";
-const firebase_database_url = "https://my-wallet-9438d.firebaseio.com";
