23bc55c4cc0862661a16b13fb23c44df92dcb413
suesitran <suesi.tran@cognizant.com>
2018-12-13 11:12:31 +1100

    Summary: Clear shared preference upon logout

--- lib/ui/home/drawer/data/drawer_repository.dart
+++ lib/ui/home/drawer/data/drawer_repository.dart
@@ -1,8 +1,16 @@
 import 'package:my_wallet/ca/data/ca_repository.dart';
 import 'package:my_wallet/data/firebase_manager.dart' as fm;
+import 'package:my_wallet/shared_pref/shared_preference.dart';
 
 class LeftDrawerRepository extends CleanArchitectureRepository {
-  Future<bool> signOut() {
-    return fm.signOut();
+  Future<bool> signOut() async {
+    bool result = await fm.signOut();
+
+    if(result) {
+      SharedPreferences pref = await SharedPreferences.getInstance();
+      pref.remove(UserUUID);
+    }
+
+    return result;
   }
 }
