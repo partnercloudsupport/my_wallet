8ab58a229ca5395870d71edea3a1af872f38d16a
suesitran <suesi.tran@cognizant.com>
2019-01-18 09:22:41 +1100

    Summary: Fix test compile error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:my_wallet/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(true, true));
+    await tester.pumpWidget(MyApp([true, true]));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
